93805ac68a60884cab78025fe9e7544c6e9b57c6
Xavier Bester <xavierbester6@gmail.com>
2025-08-17 20:19:58 -0600

    first part

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func getLinesChannels(file io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+
+	go func() {
+		defer file.Close()
+		defer close(out)
+
+		str := ""
+		for {
+			data := make([]byte, 8)
+			n, err := file.Read(data)
+			if err != nil {
+				break
+			}
+			data = data[:n]
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				str += string(data[:i])
+				data = data[i + 1:]
+				out <- str
+				str = ""
+			}
+
+			str += string(data)
+		}
+
+		if len(str) != 0 {
+			out <- str
+		}
+
+
+	}()
+
+	return out
+}
+
+func main() {
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		log.Fatal("error", "error", err)
+	}
+
+	lines := getLinesChannels(file)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
+	}
+}
